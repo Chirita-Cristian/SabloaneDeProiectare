4c5196b6daa2fe71f345e25ce72e31b38f0d0f9a
Chirita-Cristian <chirita.cristian.marian@gmail.com>
2018-11-19 16:51:55 +0200

    Laboratoare

---
+++ Lab4/src/Client.java
@@ -0,0 +1,15 @@
+
+public class Client {
+
+  public static void main(String[] args) {
+    Book noapteBuna = new Book("Noapte buna copii.!");
+    Section cap1 = new Section("Capitolul 1.");
+    cap1.add(new Paragraph("Text 1"));
+    cap1.add(new Paragraph("Text 2"));
+    cap1.add(new Image("Image 1"));
+    noapteBuna.add(cap1);
+    noapteBuna.add(new Paragraph("Multumesc"));
+    noapteBuna.print();
+  }
+
+}
---
+++ Lab4/src/Table.java
@@ -0,0 +1,29 @@
+public class Table implements Element {
+  private String title;
+
+  public Table(String title) {
+    this.title = title;
+  }
+
+  
+  public void print() {
+    System.out.println(title);
+  }
+
+  
+  public void add(Element element) {
+    System.out.println("ERORR");
+  }
+
+  
+  public void remove(Element element) {
+    System.out.println("ERORR");
+  }
+
+  
+  public Element getElement(int index) {
+    System.out.println("ERORR");
+    return null;
+  }
+
+}
